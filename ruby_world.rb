53d3913ade8219682da6212a9242e7387cb9c33e
Sravan kumar <sravan.renukntla65@gmail.com>
2022-07-19 22:36:31 +0530

    file modified

---
+++ ruby_world.rb
@@ -0,0 +1,16 @@
+# hi welcome to ruby world
+
+print"welcome to ruby world"
+puts " welcome in to my ruby world"
+puts "welcome to my home"
+
+
+=begain
+hii welcome to the ruby world
+hii welcome to the ruby world
+hii welcome to the ruby world
+hii welcome to the ruby world
+hii welcome to the ruby world
+=end
+
+
