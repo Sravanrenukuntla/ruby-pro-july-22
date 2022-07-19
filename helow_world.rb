e7051d20437f0b9f0a4e29acbfbe2bfa625bc5f9
Sravan kumar <sravan.renukntla65@gmail.com>
2022-07-19 12:52:55 +0530

    intial commiting

---
+++ helow_world.rb
@@ -0,0 +1,10 @@
+#this is a first rby sessin.
+print "hii how ru good morning"
+puts "hi helo welcome ruby world.."
+puts "hi helo welcome ruby world"
+puts "hi helow welcome to ruby world"
+
+
+
+
+	
